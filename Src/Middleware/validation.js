4576366859742041afdd1172b93b41ab3e90f0f9
RazanHarashe <harashe115@gmail.com>
2023-10-23 20:52:13 +0300

    Update validation & password

--- Src/Middleware/validation.js
+++ Src/Middleware/validation.js
@@ -1,5 +1,34 @@
+import joi from 'joi';
+import { Types } from 'mongoose';
+const dataMethods = ['body', 'query', 'params', 'headers','file']
+const validationObjectId=(value,helper)=>{
+  if(Types.ObjectId.isValid(value)){
+    return true;
+  }
+  return helper.message()
+}
+export const generalFields = {
+  id:joi.string().custom(validationObjectId).required(),
+  email: joi.string().email().required().min (5).messages({
+       'string.empty': "email is required",
+       'string.email':"plz enter a valid email"
+  }),
+   password: joi.string().required().min (3).messages({
+       'string.empty':"password is required",
+   }),
+   file:joi.object({
+      size:joi.number().positive().required(),
+      path:joi.string().required(),
+      filename:joi.string().required(),
+      destination:joi.string().required(),
+      mimetype:joi.string().required(),
+      encoding:joi.string().required(),
+      originalname:joi.string().required(),
+      fieldname:joi.string().required(),
+      dest:joi.string(),
 
-const dataMethods = ['body', 'query', 'params', 'headers']
+  })
+  }
 const validation = (Schema) => {
   return (req, res, next) => {
     const validationArray = [];
--- Src/Modules/Auth/Auth.validation.js
+++ Src/Modules/Auth/Auth.validation.js
@@ -1,13 +1,13 @@
 import joi from "joi";
-
+import { generalFields } from "../../Middleware/validation.js";
 
 export const signupSchema = {
     body: joi.object({
         userName: joi.string().alphanum().required(),
-        email: joi.string().email().required(),
+        email:generalFields.email,
         gender: joi.string().valid('Male', 'Female'),
         age: joi.number().integer().min(20).max(80).required(),
-        password: joi.string().required(),
+        password: generalFields.password,
         cpassword: joi.string().valid(joi.ref('password')).required()
     }),
    /* query: joi.object({
@@ -16,13 +16,7 @@ export const signupSchema = {
 } 
 
 export const signinSchema = joi.object({
-        email: joi.string().email().required().min(5).messages({
-            'string.empty': "email is required",
-            'string.email': "plz enter a valid email "
-        }),
-        password: joi.string().required().min(3).messages({
-    
-            'string.empty': "password is required"  
-        })
+        email: generalFields.email,
+        password: generalFields.password
     })
    
